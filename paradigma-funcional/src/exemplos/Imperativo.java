3ccc5ff2dc38ebeb81ef4f6069df3a2420ec367c
Nadia <nlnadialigia@gmail.com>
2021-07-24 17:48:34 -0300

    feat: create example of diferences between paradigmas

---
+++ paradigma-funcional/src/exemplos/Imperativo.java
@@ -0,0 +1,10 @@
+package exemplos;
+
+public class Imperativo {
+  public static void main(String[] args) {
+    int valor = 10;
+    int resultado = valor * 3;
+
+    System.out.println("O resultado é ::" + resultado);
+  }
+}
