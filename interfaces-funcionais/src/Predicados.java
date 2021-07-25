629438ea1a67b565e015f49915bb1d3955e47b1c
Nadia <nlnadialigia@gmail.com>
2021-07-25 12:10:31 -0300

    feat: create example of Predicate

---
+++ interfaces-funcionais/src/Predicados.java
@@ -0,0 +1,11 @@
+import java.util.function.Predicate;
+
+public class Predicados {
+  public static void main(String[] args) {
+
+    //Predicate<String> estaVazio = valor -> valor.isEmpty();
+    Predicate<String> estaVazio = String::isEmpty;
+    System.out.println(estaVazio.test(""));
+    System.out.println(estaVazio.test("Liberdade"));
+  }
+}
