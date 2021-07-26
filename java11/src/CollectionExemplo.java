15f16f1c40cabcb73ec5484c88ad4f247c41066d
Nadia <nlnadialigia@gmail.com>
2021-07-25 23:32:17 -0300

    feat: create java 11 package

---
+++ java11/src/CollectionExemplo.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+public class CollectionExemplo {
+
+    public static void main(String[] args) {
+        Collection<String> nomes = Set.of("Joao", "Paulo", "Oliveira", "Santos");
+
+        System.out.println(nomes);
+    }
+}
